4e6631c79bb7b6958583b34a690615df14475554
Hemingway <ghostblac@protonmail.com>
2021-06-07 15:07:01 -0400

    Add files via upload

---
+++ persephone-hesitated.py
@@ -0,0 +1,379 @@
+import datetime
+from time import sleep
+
+opening = [
+    "PERSEPHONE HESITATED",
+    "© 2021 demiurge"
+]
+
+for i in range(2):
+    print(opening[i])
+    sleep(0.381)
+
+name = input("What is your name?: ")
+
+persephone = [
+    f"Wake up, {name}...",
+    "Carcosa has you...",
+    "Follow the yellow sign.",
+    f"Check your messages, {name}."
+]
+
+for i in range(4):
+    print(persephone[i])
+    sleep(11.25)
+
+# routing: 603432324
+
+# account: 61946872427
+
+choice1 = input("(check / decline / ls): ")
+
+if choice1 == "check":
+
+    yes = [
+        "knowsbiercing42: Persephone?",
+    ]
+
+    for i in range(1):
+        print(yes[i])
+        sleep(0.381)
+
+    yes2 = [
+        "knowsbiercing42: For some reason my first thought was Bellucci's character from The Matrix sequels.",
+        "knowsbiercing42: Hmm, strange text on your computer, could still be Bellucci.",
+        "knowsbiercing42: Makes sense to me."
+    ]
+
+    for i in range(3):
+        print(yes2[i])
+        sleep(6.1)
+
+    choice2 = input("(Bellucci? / The Greek myth / The yellow sign): ")
+
+    if choice2 == "Bellucci?":
+
+        bellucci = [
+            "knowsbiercing42: The Italian actress?",
+            "knowsbiercing42: A Burning Hot Summer? The Apartment? Spectre?",
+            "knowsbiercing42: The afermentioned Matrix sequels?",
+            "knowsbiercing42: I'm sure you have the internet. Look her up."
+        ]
+
+        for i in range(4):
+            print(bellucci[i])
+            sleep(2)
+
+        choice3 = input("(The Greek myth / The yellow sign): ")
+
+                if choice7
+                    question8
+                        if choice15
+                            question16
+                                if choice31
+                                    question32
+                                        if choice31
+                                            ending1
+                                        if choice32
+                                            ending2
+                                if choice32
+                                    question33
+                                        if choice33
+                                            ending3
+                                        if choice34
+                                            ending4
+                        if choice16
+                            question17
+                                if choice33
+                                    question34
+                                        if choice35
+                                            ending
+                                        if choice
+                                            ending
+
+                                if choice34
+                                    question35
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                if choice8
+                    question9
+                        if choice17
+                            question18
+                                if choice35
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice36
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                        if choice18
+                            question19
+                                if choice37
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice38
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+
+    elif choice2 == "The Greek myth":
+
+        the_greek_myth = [
+            "knowsbiercing42: Demeter's daughter. Hades' wife.",
+            "knowsbiercing42: She was 'given' to Hades by Zeus. The usual women-as-property bullshit.",
+            "knowsbiercing42: A lot of the company's properties are named after Greek myths and legends.",
+            "knowsbiercing42: Do you think the messages were about the myth or the company property?"
+        ]
+
+        for i in range(4):
+            print(the_greek_myth[i])
+            sleep(1)
+
+        choice3 = input("Demeter / other company properties / Persephone program): ")
+
+                if chpice9
+                    question10
+                        if choice19
+                            question20
+                                if choice39
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice40
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                        if choice20
+                            question21
+                                if choice41
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice42
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                if choice10
+                    question11
+                        if choice21
+                            question22
+                                if choice43
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice44
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                        if choice22
+                            question23
+                                if choice45
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice46
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+
+    elif choice2 == "The yellow sign":
+
+        the_yellow_sign = [
+            "knowsbiercing42: From the Chambers book.",
+            "knowsbiercing42: So the messages mentioned Carcosa AND the yellow sign?",
+            "knowsbiercing42: And you just woke up at your desk and found them waiting for you?",
+            "knowsbiercing42: Odd."
+        ]
+
+        for i in range(4):
+            print(the_yellow_sign[i])
+            sleep(1)
+
+        choice3 = input("[ask] if she sent the messages / [accuse] her sending the messages / [change] subject")
+
+    else:
+        print(f"knowsbiercing42: What the fuck is {choice2}? You smoke a huge bowl earlier?")
+
+elif choice1 == "ls":
+
+    list_files = [
+        "README.md",
+        "v1.8.8.61225_release_notes"
+    ]
+
+    for i in range(2):
+        print(list_files[i])
+
+    choice2 = input("cat [file name]: ")
+
+    if choice2 == "cat README.md":
+            question4
+                if choice7
+                    question8
+                        if choice15
+                            question16
+                                if choice31
+                                    question32
+                                        if choice31
+                                            ending1
+                                        if choice32
+                                            ending2
+                                if choice32
+                                    question33
+                                        if choice33
+                                            ending3
+                                        if choice34
+                                            ending4
+                        if choice16
+                            question17
+                                if choice33
+                                    question34
+                                        if choice35
+                                             ending
+                                        if choice
+                                            ending
+
+                                if choice34
+                                    question35
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                if choice8
+                    question9
+                        if choice17
+                            question18
+                                if choice35
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice36
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                        if choice18
+                            question19
+                                if choice37
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice38
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+        elif choice2 == "cat v1.8.8.61225_release_notes":
+            question5
+                if chpice9
+                    question10
+                        if choice19
+                            question20
+                                if choice39
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice40
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                        if choice20
+                            question21
+                                if choice41
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice42
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                if choice10
+                    question11
+                        if choice21
+                            question22
+                                if choice43
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice44
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                        if choice22
+                            question23
+                                if choice45
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+                                if choice46
+                                    question
+                                        if choice
+                                            ending
+                                        if choice
+                                            ending
+        else:
+            print(f"You do not have permission to use this command: {choice2}")
+
+elif choice1 == "decline":
+    print("You unplugged your terminal and went for a walk.")
+
+else:
+    print("The terminal shut off.")
---
+++ untitled.py
@@ -0,0 +1,56 @@
+"1886, An Inhabitant in Carcosa",
+"1891, Haïta the Shepherd",
+"1895, The Repairer of Reputations",
+"1895, The Mask",
+"1895, In the Court of the Dragons",
+"1895, The Yellow Sign"
+
+Demeter + Persephone myth
+
+"Where Time Shadows" + "Federal Bureau of Control in the Oldest House"
+
+1 player character
+4 AIs
+28 NPCs / enemies
+
+Automatons / Genshin Impact
+
+"""
+Programmer simulator: A simulation video game describes a diverse super-category of video games,
+generally designed to closely simulate real world activities. A simulation game attempts to copy
+various activities from real life in the form of a game for various purposes such as training,
+analysis, or prediction. Usually there are no strictly defined goals in the game, with the player
+instead allowed to control a character or environment freely.
+
+interactive fiction: Interactive fiction, often abbreviated IF, is software simulating environments
+in which players use text commands to control characters and influence the environment. Works in
+this form can be understood as literary narratives, either in the form of interactive narratives or
+interactive narrations. These works can also be understood as a form of video game, either in the
+form of an adventure game or role-playing game.
+
+platformer: Cinematic platformers are a small but distinct subgenre of platform games, usually
+distinguished by their relative realism compared to traditional platformers. These games focus on
+fluid, lifelike movements, without the unnatural physics found in nearly all other platform games.
+To achieve this realism, many cinematic platformers, beginning with Prince of Persia, have employed
+rotoscoping techniques to animate their characters based on video footage of live actors performing
+the same stunts.
+
+Game story:
+
+Space traveler Persephone awakens in a strange place after her ship sustains heavy damage and
+crashes. Following the crash, Persephone is contacted by the ship's onboard AI, Dee. Along with
+Dee, Persephone travels through the alien forest in an effort to find her ship, and must go through
+the process of repairing Dee's damaged code. During Persephone's journey, she is presented with
+puzzles to better understand her predicament, and also to figure out how to fix Dee. Away from her
+ship, Dee must relying on nothing but herself to survive.
+
+* Since Persephone has no weapons, there is no way to shoot any approaching enemies.
+
+* Persephone crashed on an alien world, so enemies can be anywhere and anything.
+
+* Because the planet is alien, gravity can be different, which may affect Persephone’s jump and
+movement abilities.
+
+*Persephone needs to fix their damaged spaceship, which requires collecting items to do so. Right
+now, coins are available, but other items may be available later.
+"""
